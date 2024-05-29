300e088ada69e9b47f9c1374775e0f8e45cd96ec
Felix <felixsavero78@gmail.com>
2024-05-29 14:33:08 +0700

    feat: generic without constructor

--- tests/generic.test.ts
+++ tests/generic.test.ts
@@ -1,4 +1,5 @@
 describe('generic', () => {
+  // 1. Basic Generic
   class GenericData<T> {
     value: T;
     constructor(value: T) {
@@ -26,6 +27,7 @@ describe('generic', () => {
     expect(dataString.value.toUpperCase()).toBe('XILEF');
   });
 
+  // 2. Generic function
   it('should support generic data type', () => {
     function create<T>(data: T): T {
       return data;
@@ -42,6 +44,7 @@ describe('generic', () => {
     expect(dataGeneric.getValue()).toBe('Xilef');
   });
 
+  // 3. Multiple generic parameter
   class Entry<K, V> {
     constructor(public key: K, public value: V) {}
   }
@@ -63,5 +66,29 @@ describe('generic', () => {
     expect(triple.first).toBe('NestJS');
     expect(triple.second).toBe('Back-End');
     expect(triple.third).toBe(true);
+  });
+
+  // 4. Optional generic (with constructor parameter)
+  it('should support optional generic type', () => {
+    const entry = new Entry(1, 'Felix'); // can autodetect if defined in constructor
+    expect(typeof entry.key).toBe('number');
+    expect(typeof entry.value).toBe('string');
+  });
+
+  // 5. Generic without constructor
+  class SimpleGeneric<T> {
+    private value?: T;
+    setValue(value: T) {
+      this.value = value;
+    }
+    getValue(): T | undefined {
+      return this.value;
+    }
+  }
+
+  it('should state the generic data type', () => {
+    const simpleGeneric = new SimpleGeneric(); // not really recommended
+    simpleGeneric.setValue('Felix');
+    expect(typeof simpleGeneric.getValue()).toBe('string');
   });
 });
