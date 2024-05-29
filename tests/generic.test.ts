344ed34add6e7de3bc3f1dfba97085ebe29e6811
Felix <felixsavero78@gmail.com>
2024-05-29 13:43:07 +0700

    feat: multiple generic parameter

--- tests/generic.test.ts
+++ tests/generic.test.ts
@@ -41,4 +41,27 @@ describe('generic', () => {
     dataGeneric.setValue('Xilef');
     expect(dataGeneric.getValue()).toBe('Xilef');
   });
+
+  class Entry<K, V> {
+    constructor(public key: K, public value: V) {}
+  }
+
+  class Triple<K, V, T> {
+    constructor(public first: K, public second: V, public third: T) {}
+  }
+
+  it('should support multiple generic parameter', () => {
+    const entry = new Entry<string, string>('React.js', 'Front-End');
+    expect(entry.key).toBe('React.js');
+    expect(entry.value).toBe('Front-End');
+
+    const triple = new Triple<string, string, boolean>(
+      'NestJS',
+      'Back-End',
+      true,
+    );
+    expect(triple.first).toBe('NestJS');
+    expect(triple.second).toBe('Back-End');
+    expect(triple.third).toBe(true);
+  });
 });
